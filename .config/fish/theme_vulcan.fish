27e612f94e59a0019ac66486a39cc45681808b64
Euphorlc <euphoric.dock@gmail.com>
2026-09-02 14:22:24 +0800

    Change color palette of vulcan theme configuration files

--- .config/fish/theme_vulcan.fish
+++ .config/fish/theme_vulcan.fish
@@ -1,35 +1,35 @@
 # ~/.config/fish/theme_vulcan.fish
 
 # Primary syntax highlighting
-set -g fish_color_normal normal
-set -g fish_color_command green
-set -g fish_color_keyword green
-set -g fish_color_quote magenta      # Neon Purple
-set -g fish_color_redirection red
-set -g fish_color_end yellow         # Orange
-set -g fish_color_error brwhite
-set -g fish_color_param blue         # Light Purple
-set -g fish_color_comment brblack
-set -g fish_color_operator red
-set -g fish_color_escape red
+set -g fish_color_normal DAF2E9          # Pale Ice Mint
+set -g fish_color_command 95E0CC         # Bright Seafoam Green
+set -g fish_color_keyword 95E0CC         # Bright Seafoam Green
+set -g fish_color_quote 39707A           # Muted Teal
+set -g fish_color_redirection F14E52     # Bright Coral Red
+set -g fish_color_end 23495D             # Deep Slate Cyan
+set -g fish_color_error F14E52 --bold    # Bright Coral Red
+set -g fish_color_param DAF2E9           # Pale Ice Mint
+set -g fish_color_comment 39707A         # Muted Teal
+set -g fish_color_operator F14E52        # Bright Coral Red
+set -g fish_color_escape F14E52          # Bright Coral Red
 set -g fish_color_valid_path --underline
 
 # Autosuggestions and selection
-set -g fish_color_autosuggestion brblack
-set -g fish_color_selection white --bold --background=brblack
-set -g fish_color_search_match yellow --background=brblack
+set -g fish_color_autosuggestion 39707A
+set -g fish_color_selection DAF2E9 --bold --background=23495D
+set -g fish_color_search_match 1C2638 --background=95E0CC
 set -g fish_color_history_current --bold
 set -g fish_color_cancel -r
 
 # Pager (tab-completion menu) colors
 set -g fish_pager_color_background normal
-set -g fish_pager_color_prefix green --bold --underline
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description blue
-set -g fish_pager_color_progress brwhite --background=magenta
+set -g fish_pager_color_prefix 95E0CC --bold --underline
+set -g fish_pager_color_completion DAF2E9
+set -g fish_pager_color_description 39707A
+set -g fish_pager_color_progress DAF2E9 --background=23495D
 
-# Custom red highlight for selected items
-set -g fish_pager_color_selected_background --background=red
-set -g fish_pager_color_selected_prefix white --bold
-set -g fish_pager_color_selected_completion white --bold
-set -g fish_pager_color_selected_description brwhite
+# Crimson highlight for selected items
+set -g fish_pager_color_selected_background --background=9B222B
+set -g fish_pager_color_selected_prefix DAF2E9 --bold
+set -g fish_pager_color_selected_completion DAF2E9 --bold
+set -g fish_pager_color_selected_description 95E0CC
